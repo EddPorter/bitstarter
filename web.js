d2a5bc9b4821fc7f20f276dfacb7956be6860664
Edd Porter <github@eddporter.com>
2013-07-04 17:34:20 +0000

    Convert buffer to string

--- web.js
+++ web.js
@@ -6,7 +6,8 @@ var app = express.createServer(express.logger());
 app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
   var file = fs.readFileSync("index.html");
-  response.send(file);
+  var output = file.toString();
+  response.send(output);
 });
 
 var port = process.env.PORT || 5000;
